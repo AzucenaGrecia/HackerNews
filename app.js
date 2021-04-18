6040295effb19352a6f8e04a552ddcc2aff9ff0a
Grecia Delgado <greciadm2596@gmail.com>
2021-04-18 12:51:33 -0500

    The conection with the Mongo DB is done but the schema of collection is not defined yet

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-fetch('https://google.com')
-    .then(res => res.json())
-    .then(text => console.log(text))
+// pide data una sola vez
+const url = "http://hn.algolia.com/api/v1/search_by_date?query=nodejs";
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    console.log(json);
+  } catch (err) {
+    console.log(error);
+  }
+}
+
+//getData(url);
+
+
+// Pide la data cada X tiempo 
+// function myFunc(url) {
+
+//   async function getData(url) {
+//     try {
+//       const response = await fetch(url);
+//       const json = await response.json();
+//       console.log(json);
+//     } catch (err) {
+//       console.log(error);
+//     }
+//   }
+
+//   getData(url)
+// }
+
+//setInterval(myFunc, 5000, "http://hn.algolia.com/api/v1/search_by_date?query=nodejs");
+
+
+// conectarse con Mongo DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("conected to DB!");
+  }
+)
