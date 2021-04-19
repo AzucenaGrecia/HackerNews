aa522b8d59201dc84fe59b373b7cce7e6e923824
Grecia Delgado <greciadm2596@gmail.com>
2021-04-19 17:41:27 -0500

    add react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { css, Global } from "@emotion/react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: Raleway;
+          }
+        `}
+      />
+      <Router>
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
       <h1>HOLA!</h1>
     </div>
   );
