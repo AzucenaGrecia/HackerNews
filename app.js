16636271eb26e95c80db8f7b62e1dd6b26355890
Grecia Delgado <greciadm2596@gmail.com>
2021-04-19 12:03:32 -0500

    Now the api call from hn.algolia.com  occurs each our and save the data into Atlas MOngoDb

--- app.js
+++ app.js
@@ -7,60 +7,84 @@ const client = new MongoClient(process.env.DB_CONNECTION, {
 });
 
 // pide data una sola vez
-const url = "http://hn.algolia.com/api/v1/search_by_date?query=nodejs";
-const dbName = "ReignProyect";
+// const url = "http://hn.algolia.com/api/v1/search_by_date?query=nodejs";
+//const dbName = "ReignProyect";
+// async function getData(url) {
+//   try {
+//     const response = await fetch(url);
+//     const json = await response.json();
 
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    const news = json.hits;
-    const arrayUnqNews = [
-      ...new Map(
-        news.map((item) => [
-          item["story_id"],
-          {
-            story_id: item.story_id,
-            story_title: item.story_title,
-            story_url: item.story_url,
-            author: item.author,
-            created_at: item.created_at,
-          },
-        ])
-      ).values(),
-    ];
-    // tendria que hacerse el llenado de datos
-    await client.connect();
-    console.log("Connected correctly to server");
-    const db = client.db(dbName);
-    const col = db.collection("news");
-    const p = await col.insertMany(arrayUnqNews);
-  } catch (err) {
-    console.log(err.stack);
-  } finally {
-    await client.close();
-  }
-}
-
-getData(url);
-
-// Pide la data cada X tiempo
-// function myFunc(url) {
-
-//   async function getData(url) {
-//     try {
-//       const response = await fetch(url);
-//       const json = await response.json();
-//       console.log(json);
-//     } catch (err) {
-//       console.log(error);
-//     }
+//     const news = json.hits;
+//     const arrayUnqNews = [
+//       ...new Map(
+//         news.map((item) => [
+//           item["story_id"],
+//           {
+//             story_id: item.story_id,
+//             story_title: item.story_title,
+//             story_url: item.story_url,
+//             author: item.author,
+//             created_at: item.created_at,
+//           },
+//         ])
+//       ).values(),
+//     ];
+//     // tendria que hacerse el llenado de datos
+//     await client.connect();
+//     console.log("Connected correctly to server");
+//     const db = client.db(dbName);
+//     const col = db.collection("news");
+//     const p = await col.insertMany(arrayUnqNews);
+//   } catch (err) {
+//     console.log(err.stack);
+//   } finally {
+//     await client.close();
 //   }
-
-//   getData(url)
 // }
 
-//setInterval(myFunc, 5000, "http://hn.algolia.com/api/v1/search_by_date?query=nodejs");
+// getData(url);
+
+
+// Pide la data cada X tiempo
+function myFunc(url) {
+  async function getData(url) {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      const news = json.hits;
+      const arrayUnqNews = [
+        ...new Map(
+          news.map((item) => [
+            item["story_id"],
+            {
+              story_id: item.story_id,
+              story_title: item.story_title,
+              story_url: item.story_url,
+              author: item.author,
+              created_at: item.created_at,
+            },
+          ])
+        ).values(),
+      ];
+      // tendria que hacerse el llenado de datos
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db("ReignProyect");
+      const col = db.collection("news");
+      const p = await col.insertMany(arrayUnqNews);
+    } catch (err) {
+      console.log(err.stack);
+    } 
+  }
+
+  getData(url);
+}
+
+setInterval(
+  myFunc,
+  1000*60*60*1,
+  "http://hn.algolia.com/api/v1/search_by_date?query=nodejs"
+);
 
 //conectarse con Mongo DB
 // mongoose.connect(
