dcbf03300dd17b74da02ddb9314878ed617794c4
Grecia Delgado <greciadm2596@gmail.com>
2021-04-19 16:19:37 -0500

    Configure some application-level middleware for our server and set out server to listen on port 5000

--- app.js
+++ app.js
@@ -49,9 +49,44 @@ setInterval(
   1000 * 60 * 60 * 1,
   "http://hn.algolia.com/api/v1/search_by_date?query=nodejs"
 );
-// Midlewares
-app.use(cors());
+
+// listen to the server
+// app.listen(5000);
+const port = process.env.PORT || 5000;
+
+// This application level middleware prints incoming requests to the servers console, useful to see incoming requests
+app.use((req, res, next) => {
+  console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+  next();
+});
+
+// Configure the bodyParser middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Configure the CORs middleware
+app.use(cors());
+
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+};
+
+// Catch any bad requests
+app.get('*', (req, res) => {
+  res.status(200).json({
+      msg: 'Catch All'
+  });
+});
+
+// Configure our server to listen on the port defiend by our port variable
+app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
 
 // Import Routes
 const newsRoute = require("./routes/news");
@@ -65,5 +100,3 @@ mongoose.connect(
     console.log("conected to DB!");
   }
 );
-// listen to the server
-app.listen(5000);
