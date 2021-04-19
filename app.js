8e6c83de2843da6fd3a6644f5db3e4f888430fa8
Grecia Delgado <greciadm2596@gmail.com>
2021-04-19 12:19:20 -0500

    use express to be able to listen by port 3000

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const app = express();
 const fetch = require("node-fetch");
 const mongoose = require("mongoose");
 require("dotenv/config");
@@ -6,46 +8,7 @@ const client = new MongoClient(process.env.DB_CONNECTION, {
   useUnifiedTopology: true,
 });
 
-// pide data una sola vez
-// const url = "http://hn.algolia.com/api/v1/search_by_date?query=nodejs";
-//const dbName = "ReignProyect";
-// async function getData(url) {
-//   try {
-//     const response = await fetch(url);
-//     const json = await response.json();
-
-//     const news = json.hits;
-//     const arrayUnqNews = [
-//       ...new Map(
-//         news.map((item) => [
-//           item["story_id"],
-//           {
-//             story_id: item.story_id,
-//             story_title: item.story_title,
-//             story_url: item.story_url,
-//             author: item.author,
-//             created_at: item.created_at,
-//           },
-//         ])
-//       ).values(),
-//     ];
-//     // tendria que hacerse el llenado de datos
-//     await client.connect();
-//     console.log("Connected correctly to server");
-//     const db = client.db(dbName);
-//     const col = db.collection("news");
-//     const p = await col.insertMany(arrayUnqNews);
-//   } catch (err) {
-//     console.log(err.stack);
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// getData(url);
-
-
-// Pide la data cada X tiempo
+// request to HACKER NEWS API each hour
 function myFunc(url) {
   async function getData(url) {
     try {
@@ -66,15 +29,14 @@ function myFunc(url) {
           ])
         ).values(),
       ];
-      // tendria que hacerse el llenado de datos
       await client.connect();
-      console.log("Connected correctly to server");
       const db = client.db("ReignProyect");
       const col = db.collection("news");
       const p = await col.insertMany(arrayUnqNews);
+      console.log("Data was stored into Mongo DB");
     } catch (err) {
       console.log(err.stack);
-    } 
+    }
   }
 
   getData(url);
@@ -82,25 +44,17 @@ function myFunc(url) {
 
 setInterval(
   myFunc,
-  1000*60*60*1,
+  1000 * 60 * 60 * 1,
   "http://hn.algolia.com/api/v1/search_by_date?query=nodejs"
 );
 
-//conectarse con Mongo DB
-// mongoose.connect(
-//   process.env.DB_CONNECTION,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log("conected to DB!");
-//   }
-// )
-
-// conection with mongo database
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://testguy:Google2349@cluster0.vk6rw.mongodb.net/ReignProyect?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+// connection with Mongo DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("conected to DB!");
+  }
+)
+// listen to the server 
+app.listen(3000)
