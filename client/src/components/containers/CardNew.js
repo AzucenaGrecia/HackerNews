52761c5d89a3e01e664868ff6337648924459f4f
Grecia Delgado <greciadm2596@gmail.com>
2021-04-21 17:11:54 -0500

    The Date of the News, now have the correct format

--- client/src/components/containers/CardNew.js
+++ client/src/components/containers/CardNew.js
@@ -1,13 +1,27 @@
 import styled from "@emotion/styled";
+import { differenceInDays, format } from "date-fns";
 
 export default function CardNew({ children, author, date, onClick }) {
+  let newDate = new Date(date);
+  let result = differenceInDays(Date.now(), newDate);
+  let formatDate = null;
+
+  if (result <= 1) {
+    formatDate = format(newDate, "HH:mm aaaa");
+    console.log(formatDate);
+  } else if (result === 2) {
+    formatDate = "Yesterday";
+  } else {
+    formatDate = format(newDate, "dd LLL");
+  }
+
   return (
     <StyledCard onClick={onClick}>
       <div>
         {children}
         <span>- {author} -</span>
       </div>
-      <div>{date}</div>
+      <div>{formatDate}</div>
     </StyledCard>
   );
 }
