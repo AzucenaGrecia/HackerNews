5a690a485e6d757853b6ccebe3bbb7aedab04ca4
Grecia Delgado <greciadm2596@gmail.com>
2021-04-21 13:31:54 -0500

    Add Global Styles into file index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,14 @@
 import React from "react";
 import { css, Global } from "@emotion/react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Global
-        styles={css`
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: Raleway;
-          }
-        `}
-      />
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
         </Switch>
       </Router>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { css, Global } from "@emotion/react";
+const GlobalStyles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Balsamiq Sans;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={GlobalStyles}/>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
